c9bf3d68663288e6783578c6d07d0e303a28fa4e
Amirhossein Akbari <amirhossein.akbari@dci-student.org>
2025-06-02 21:34:16 +0200

    filter finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import HomePage from "./routes/homePage/homePage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
 
 function App() {
   const router = createBrowserRouter([
@@ -9,6 +12,7 @@ function App() {
       children: [
         { path: "/", element: <HomePage /> },
         { path: "/list", element: <ListPage /> },
+        { path: "/:id", element: <SinglePage /> },
       ],
     },
   ]);
